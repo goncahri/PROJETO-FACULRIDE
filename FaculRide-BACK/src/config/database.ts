cb349e255cbeb3299307a25605b9b00bcfbe94af
Herivelton Gonçalves <herivelton02@gmail.com>
2025-06-23 00:09:23 -0300

    ajuste swagger

--- FaculRide-BACK/src/config/database.ts
+++ FaculRide-BACK/src/config/database.ts
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   protocol: "postgres",
   dialectOptions: {
     ssl: {
-      require: false,
+      require: true,
       rejectUnauthorized: false
     }
   },
